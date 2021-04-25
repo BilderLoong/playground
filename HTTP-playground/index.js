335a3a51b49e57ee402e5b26b879d6d05bb5caab
bilder <514059172@163.com>
2021-04-25 19:37:10 +0800

    add event demo

--- HTTP-playground/index.js
+++ HTTP-playground/index.js
@@ -1,8 +1,32 @@
-(async function () {
-  try {
-    const result = await fetch('http://localhost:81');
-    console.log(result);
-  } catch (err) {
-    console.error(err);
-  }
+const customEvent = new Event('custom', {
+  bubbles: false,
+  cancelable: false,
+});
+
+const button = document.querySelector('button');
+button.addEventListener('click', (e) => {
+  console.log(e.isTrusted);
+});
+
+button.addEventListener('custom', (e) => {
+  console.log(e);
+});
+
+button.dispatchEvent(customEvent);
+
+// Event target
+(function () {
+  const eventTarget = new EventTarget();
+  const customEvent = new CustomEvent('sleep', {
+    detail: {
+      info: 'info',
+    },
+  });
+
+  eventTarget.addEventListener('sleep', (e) => {
+    console.log(e.detail.info);
+  });
+
+  eventTarget.dispatchEvent(customEvent);
+  console.log(eventTarget);
 })();
