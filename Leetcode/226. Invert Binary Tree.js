71eaf6f0bf2ee8824439ac9ea112cf7509e0e45c
BilderLoong <cokoryuu@gmail.com>
2021-05-05 11:20:52 +0800

    clear

--- Leetcode/226. Invert Binary Tree.js
+++
@@ -1,8 +0,0 @@
-// Recursion
-var invertTree = function (root) {
-  if (root === null) return root;
-  invertTree(root.left);
-  invertTree(root.right);
-  [root.left, root.right] = [root.right, root.left];
-  return root;
-};
