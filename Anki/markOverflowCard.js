11dbd3d11cb266a254ca74229bae19a0dc3139e9
BilderLoong <cokoryuu@gmail.com>
2021-07-10 11:54:19 +0800

    complete the markOverflowCard

--- Anki/markOverflowCard.js
+++ Anki/markOverflowCard.js
@@ -1,23 +1,28 @@
 markOverflowCard();
 function markOverflowCard() {
-  ankiShowToast(`markOverflowCard run`);
+	// Change to toggle whether show the toast.
+  const DEBUG = false;
+
   if (!_canUseToggleFlag()) {
-    ankiShowToast(`can't use toggle flag`);
+    toast(`can't use toggle flag`);
     return;
   }
 
   const BLUE = 4;
   const NONE = 0;
 
+  toast(`Flag: ${AnkiDroidJS.ankiGetCardFlag()}`);
+  toast(`_isNeedScroll(): ${_isNeedScroll()}`);
+
   if (AnkiDroidJS.ankiGetCardFlag() !== BLUE && _isNeedScroll()) {
     // blue flag means this cart has overflow content.
     ankiToggleFlag(BLUE);
-    ankiShowToast(`Add blue flag`);
+    toast(`Add blue flag`);
   }
 
   if (AnkiDroidJS.ankiGetCardFlag() === BLUE && !_isNeedScroll()) {
     ankiToggleFlag(NONE);
-    ankiShowToast(`Remove blue flag`);
+    toast(`Remove blue flag`);
   }
 
   /**
@@ -25,17 +30,38 @@ function markOverflowCard() {
    */
   function _isNeedScroll() {
     const bodyWidth = document.body.clientWidth;
-    const viewPortWidth = document.documentElement.clientWidth;
+    const bodyScrollWidth = document.body.scrollWidth;
+    // const viewPortWidth = document.documentElement.clientWidth;
+    // const viewPortScrollWidth = document.documentElement.scrollWidth;
 
-    return bodyWidth > viewPortWidth;
+    // toast(`viewPortScrollWidth: ${viewPortScrollWidth}`);
+    // toast(`viewPortWidth: ${viewPortWidth}`);
+    // toast(`bodyScrollWidth: ${bodyScrollWidth}`);
+    // toast(`bodyWidth: ${bodyWidth}`);
+    // toast(`window.inner: ${window.innerWidth}`);
+    const res = bodyScrollWidth > bodyWidth;
+    toast(res);
+    return res;
   }
 
   /**
-   *
+   * @returns {boolean}
+   */
+  function _canUseAnkiDroidJS() {
+    try {
+      AnkiDroidJS;
+    } catch (error) {
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
    * @returns {boolean}
    */
   function _canUseToggleFlag() {
-    ankiShowToast(`_canUseToggleFlag run`);
+    toast(`_canUseToggleFlag run`);
     try {
       // To use toggleFlag api need to initialize first: https://github.com/ankidroid/Anki-Android/wiki/AnkiDroid-Javascript-API#initialize
       const apiStatus = AnkiDroidJS.init(
@@ -45,16 +71,25 @@ function markOverflowCard() {
       const api = JSON.parse(apiStatus);
 
       if (!api['toggleFlag']) {
-        ankiShowToast(`can't use api[toggleFlag]`);
+        toast(`can't use api[toggleFlag]`);
         return false;
       }
     } catch (error) {
-      ankiShowToast(error);
-      ankiShowToast(`can't use AnkiDroidJS`);
+      toast(`can't use AnkiDroidJS`);
       return false;
     }
+
+    toast(`able to use AnkiDroidJS`);
+    return true;
   }
 
-  ankiShowToast(`able to use AnkiDroidJS`);
-  return true;
+  /**
+   * @param {string} msg
+   * @return {void}
+   */
+  function toast(msg) {
+    if (_canUseAnkiDroidJS() && DEBUG) {
+      ankiShowToast(msg.toString());
+    }
+  }
 }
