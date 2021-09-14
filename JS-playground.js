221398e22dfc02c8de038b3174a63d7ed04ff4a0
huzilong <>
2021-09-14 09:48:00 +0800

    save

--- JS-playground.js
+++ JS-playground.js
@@ -1,60 +1,92 @@
-markOverflowCard();
-function markOverflowCard() {
-  ankiShowToast(`markOverflowCard run`);
-  if (!_canUseToggleFlag()) {
-    ankiShowToast(`can't use toggle flag`);
-    return;
+try {
+  
+} catch (error) {
+  
+}
+try {
+  console.log(Symbol.for('id') === Symbol.for('id'));
+  console.log(Symbol.for('id') === Symbol('id'));
+} catch (error) {}
+try {
+  const bar = {
+    name: 1,
+    age: undefined,
+  };
+
+  console.log({ ...bar, name: 2 });
+} catch (error) {}
+try {
+  function foo({ name, age } = {}) {
+    const obj = {
+      name,
+      age,
+    };
+
+    console.log({ ...obj });
   }
 
-  const BLUE = 4;
-  const NONE = 0;
+  foo({ name: 1, height: 4 });
+  // foo({ name: 1, age: 4 });
+} catch (error) {}
+try {
+  markOverflowCard();
+  function markOverflowCard() {
+    ankiShowToast(`markOverflowCard run`);
+    if (!_canUseToggleFlag()) {
+      ankiShowToast(`can't use toggle flag`);
+      return;
+    }
 
-  if (AnkiDroidJS.ankiGetCardFlag() !== BLUE && _isNeedScroll()) {
-    // blue flag means this cart has overflow content.
-    ankiToggleFlag(BLUE);
-    ankiShowToast(`Add blue flag`);
-  }
+    const BLUE = 4;
+    const NONE = 0;
 
-  if (AnkiDroidJS.ankiGetCardFlag() === BLUE && !_isNeedScroll()) {
-    ankiToggleFlag(NONE);
-    ankiShowToast(`Remove blue flag`);
-  }
+    if (AnkiDroidJS.ankiGetCardFlag() !== BLUE && _isNeedScroll()) {
+      // blue flag means this cart has overflow content.
+      ankiToggleFlag(BLUE);
+      ankiShowToast(`Add blue flag`);
+    }
 
-  /**
-   * @returns {boolean}
-   */
-  function _isNeedScroll() {
-    const bodyWidth = document.body.clientWidth;
-    const viewPortWidth = document.documentElement.clientWidth;
+    if (AnkiDroidJS.ankiGetCardFlag() === BLUE && !_isNeedScroll()) {
+      ankiToggleFlag(NONE);
+      ankiShowToast(`Remove blue flag`);
+    }
 
-    return bodyWidth > viewPortWidth;
-  }
+    /**
+     * @returns {boolean}
+     */
+    function _isNeedScroll() {
+      const bodyWidth = document.body.clientWidth;
+      const viewPortWidth = document.documentElement.clientWidth;
 
-  /**
-   *
-   * @returns {boolean}
-   */
-  function _canUseToggleFlag() {
-    ankiShowToast(`_canUseToggleFlag run`);
-    try {
-      // To use toggleFlag api need to initialize first: https://github.com/ankidroid/Anki-Android/wiki/AnkiDroid-Javascript-API#initialize
-      const apiStatus = AnkiDroidJS.init(
-        JSON.stringify({ version: '0.0.1', developer: 'cokoryu@outlook.com' })
-      );
+      return bodyWidth > viewPortWidth;
+    }
 
-      const api = JSON.parse(apiStatus);
+    /**
+     *
+     * @returns {boolean}
+     */
+    function _canUseToggleFlag() {
+      ankiShowToast(`_canUseToggleFlag run`);
+      try {
+        // To use toggleFlag api need to initialize first: https://github.com/ankidroid/Anki-Android/wiki/AnkiDroid-Javascript-API#initialize
+        const apiStatus = AnkiDroidJS.init(
+          JSON.stringify({ version: '0.0.1', developer: 'cokoryu@outlook.com' })
+        );
 
-      if (!api['toggleFlag']) {
-        ankiShowToast(`can't use api[toggleFlag]`);
+        const api = JSON.parse(apiStatus);
+
+        if (!api['toggleFlag']) {
+          ankiShowToast(`can't use api[toggleFlag]`);
+          return false;
+        }
+      } catch (error) {
+        ankiShowToast(error);
+        ankiShowToast(`can't use AnkiDroidJS`);
         return false;
       }
-    } catch (error) {
-      ankiShowToast(error);
-      ankiShowToast(`can't use AnkiDroidJS`);
-      return false;
     }
-  }
 
-  ankiShowToast(`able to use AnkiDroidJS`);
-  return true;
-}
+    ankiShowToast(`able to use AnkiDroidJS`);
+    return true;
+  }
+} catch (error) {}
