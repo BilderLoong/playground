9d72fd380fa5afe72e7ab1df2d4645c4743595b6
BilderLoong <cokoryu@outlook.com>
2022-03-11 12:59:30 +0800

    save

---
+++ Leetcode/2022/Code_Interview/4.test.ts
@@ -0,0 +1,37 @@
+it('should ', () => {
+  const twoDArray1 = [
+    [1, 4, 7, 11, 15],
+    [2, 5, 8, 12, 19],
+    [3, 6, 9, 16, 22],
+    [10, 13, 14, 17, 24],
+    [18, 21, 23, 26, 30],
+  ];
+
+  expect(findNumberIn2DArray(twoDArray1, 5)).toBe(true);
+  expect(findNumberIn2DArray(twoDArray1, 20)).toBe(false);
+
+  function findNumberIn2DArray(matrix: number[][], target: number): boolean {
+    //
+  }
+});
+
+it('should ', () => {
+  const twoDArray1 = [
+    [1, 4, 7, 11, 15],
+    [2, 5, 8, 12, 19],
+    [3, 6, 9, 16, 22],
+    [10, 13, 14, 17, 24],
+    [18, 21, 23, 26, 30],
+  ];
+
+  expect(findNumberIn2DArray(twoDArray1, 5)).toBe(true);
+  expect(findNumberIn2DArray(twoDArray1, 20)).toBe(false);
+
+  function findNumberIn2DArray(matrix: number[][], target: number): boolean {
+    // Per line binary search.
+  }
+});
+
+function findNumberIn2DArray(matrix: number[][], target: number): boolean {
+  //
+}
