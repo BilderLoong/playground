1f48e2e1952ce3dd4391eca59f31b604ca215d34
BilderLoong <cokoryuu@gmail.com>
2022-10-01 01:18:16 +0800

    save

--- automatic-script/concat_gopro_video.sh
+++ automatic-script/concat_gopro_video.sh
@@ -1,15 +1,14 @@
-echo $1
 # TODO input multi file number and concat them all.
-# read -p "Please the number of videos that you want to concat: " n
+file_list_file=.${1}.tmp
 
 # https://stackoverflow.com/questions/7333232/how-to-concatenate-two-mp4-files-using-ffmpeg
 # The meaning of %P in find of -printf option https://unix.stackexchange.com/questions/215234/explanation-of-directives-in-find-printf
 # TODO learn about `find` command.
-ffmpeg_file_list=$(find . -type f -name 'G*'${n}'.MP4' -printf $'file %P\n' | sort) 
+ffmpeg_file_list=$(find . -type f -name 'G*'${1}'.MP4' -printf 'file %P\n' | sort) 
 
-echo "$ffmpeg_file_list" > temp.tmp
+echo "$ffmpeg_file_list" > $file_list_file
 
-echo ffmpeg_file_list: $(cat temp.tmp)
+echo ffmpeg_file_list: $(cat $file_list_file)
 
 
 echo begin run ffmpeg!
@@ -18,4 +17,4 @@ echo begin run ffmpeg!
 # TODO: using string replace the temp.tmp file.
 # TODO: add date information in output file name.
 # TODO mv chaptered files after concat.
-# ffmpeg -f concat -i temp.tmp -c copy $n.mkv
+ffmpeg -f concat -i $file_list_file -c copy $n.mkv
