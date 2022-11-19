7d13ba10add346281c01937d429ecbded1749d39
BilderLoong <cokoryuu@gmail.com>
2022-11-19 14:37:25 +0800

    save

--- bash-playground/playground.sh
+++ bash-playground/playground.sh
@@ -1,9 +1,11 @@
-echo foo: ${foo:+didi}
+a=123.234
+echo ${a#1}
+# echo foo: ${foo:+didi}
 
-bar=1
-echo ${bar:+didi}
+# bar=1
+# echo ${bar:+didi}
 # check_exit_staus(){
-# if [ $? -eq 0 ]; then 
+# if [ $? -eq 0 ]; then
 #   echo "success!"
 # else
 #   echo "failed!"
