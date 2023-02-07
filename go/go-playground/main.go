1bd26f8ee9071112bae8409bb49cce69a63160bf
huzilong <huzilong@meituan.com>
2023-02-07 15:58:49 +0800

    save

--- go/go-playground/main.go
+++ go/go-playground/main.go
@@ -1,28 +1,58 @@
 package main
 
 import (
-	"strings"
-
-	"golang.org/x/tour/wc"
+	"fmt"
+	"golang.org/x/tour/tree"
 )
 
-func WordCount(s string) map[string]int {
-	str_arr := strings.Fields(s)
-	count_map := make(map[string]int)
-
-	for _, word := range str_arr {
-		v := count_map[word]
-		count_map[word] = v + 1
-		// v, ok := count_map[word]
-		// if ok {
-		// 	count_map[word] = v + 1
-		// } else {
-		// 	count_map[word] = 1
-		// }
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
 	}
-	return count_map
+
+	ch <- t.Value
+
+	if t.Left != nil {
+		Walk(t.Left, ch)
+	}
+	if t.Right != nil {
+		Walk(t.Right, ch)
+	}
+
+	return
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	ch1, ch2 := make(chan int), make(chan int)
+
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+
+	for {
+		a, ok1 := <-ch1
+		b, ok2 := <-ch2
+
+		// fmt.Println(a, b)
+
+		if a != b {
+			return false
+		}
+
+		if (ok1 || ok2) == false {
+			return false
+		}
+
+	}
+
+	return true
 }
 
 func main() {
-	wc.Test(WordCount)
+	t1, t2 := tree.New(1), tree.New(1)
+	fmt.Println(Same(t1, t2))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
 }
