ff6d7c6934a9f3dc19797989bbbce0cdf75d5905
bilder <cokoryu@outlook.com>
2023-04-09 05:11:04 +0000

    save

--- temp.ts
+++ temp.ts
@@ -1,26 +1,6 @@
-import shell = require("shelljs");
+import { spawn } from "child_process";
 
-// shell.exec(
-//   "echo 1 & false & echo 2",
-//   { silent: true },
-//   (code, stdout, stderr) => {
-//     console.log("Exit code:", code);
-//     console.log("Program output:", stdout);
-//     console.log("Program stderr:", stderr);
-//   }
-// );
+const c1 = spawn("export a=1; echo $a", [], { stdio: "inherit", shell: true });
+const c2 = spawn("echo $a", [], { stdio: "inherit", shell: true });
 
-const child = shell.exec("false; >&2 echo error & echo next", {
-  async: true,
-  fatal: true,
-  silent: true,
-});
-
-// stdout?.on("data", (data) => console.log(data));
-// stderr?.on("data", (data) => {
-//   console.error(data);
-//   process.exit(1);
-// });
-// code?.on("exit", (code) => {
-//   console.log("code is:", code);
-// });
+console.log({ c2 });
