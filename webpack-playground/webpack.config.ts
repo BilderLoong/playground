c23e1ce7f8f09d80f155c1e1a5ca350c56a10d69
huzilong <huzilong@meituan.com>
2023-04-13 19:03:44 +0800

    init

---
+++ webpack-playground/webpack.config.ts
@@ -0,0 +1,36 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+
+import "webpack-dev-server";
+
+const config: webpack.Configuration = {
+  mode: "development",
+  entry: {
+    page1: "./src/page1.js",
+    page2: "./src/page2.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    // https://webpack.js.org/guides/output-management/#cleaning-up-the-dist-folder
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "page1.html",
+      chunks: ["page1"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "page2.html",
+      chunks: ["page2"],
+    }),
+  ],
+  devServer: {
+    // contentBase: path.resolve(__dirname, "dist"),
+    // static: "./dist",
+    port: 8081,
+  },
+};
+
+export default config;
