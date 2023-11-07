cd8dd6b7059a229b3d131ae3f67f64f711f53433
huzilong <huzilong@meituan.com>
2023-11-07 16:54:30 +0800

    save

--- Haskell/curry.hs
+++ Haskell/curry.hs
@@ -18,3 +18,20 @@ anonymous = \i b -> i + nonesense b
 
 anonymousNest :: Integer -> Bool -> Integer
 anonymousNest = \i -> \b -> i + nonesense b
+
+curry f (x, y) = f x y
+uncurry f x y = f (x, y)
+
+u = undefined
+
+f :: a -> a -> a -> a
+f = u
+
+x :: Char = u
+
+g :: a -> b -> c -> b
+g = u
+
+h :: (Num a, Num b) => a -> b -> b
+h = u
+h :: (Num a, Num b) => a -> b -> b
