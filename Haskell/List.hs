5cbf08e75666a69ba6c39ceb8a3715e82feb275e
huzilong <huzilong@meituan.com>
2023-11-22 13:32:45 +0800

    save

--- Haskell/List.hs
+++ Haskell/List.hs
@@ -40,7 +40,7 @@ myWord's "" = []
 myWord's str = word : myWord's rest
  where
   word = takeWhile (/= ' ') str
-  rest = dropWhile (== ' ') str
+  rest = dropWhile (== ' ') . dropWhile (/= ' ') $ str
 
 myWords :: String -> [String]
 myWords "" = [] -- Base case: empty string returns an empty list
@@ -54,3 +54,41 @@ myWords str
   word = takeWhile (/= ' ') noLeadingSpaces
   -- Drop the word and the trailing spaces, if any
   rest = dropWhile (== ' ') . dropWhile (/= ' ') $ noLeadingSpaces
+
+firstSen = "Tyger Tyger, burning bright\n"
+secondSen = "In the forests of the night\n"
+thirdSen = "What immortal hand or eye\n"
+fourthSen =
+  "Could frame thy fearful\
+  \ symmetry?"
+sentences =
+  firstSen
+    ++ secondSen
+    ++ thirdSen
+    ++ fourthSen
+shouldEqual =
+  [ "Tyger Tyger, burning bright"
+  , "In the forests of the night"
+  , "What immortal hand or eye"
+  , "Could frame thy fearful symmetry?"
+  ]
+
+myLines :: String -> [String]
+myLines str
+  | noLeadingLN == "" = []
+  | otherwise = line : myLines rest
+ where
+  noLeadingLN = dropWhile (== '\n') str
+  line = takeWhile (/= '\n') noLeadingLN
+  rest = dropWhile (/= '\n') noLeadingLN
+
+test0 = print $ "Are they equal? " ++ show (myLines sentences == shouldEqual)
+
+mySplit :: [Char] -> Char -> [String]
+mySplit str del
+  | noLeadingDelStr == "" = []
+  | otherwise = item : mySplit restStr del
+ where
+  noLeadingDelStr = dropWhile (== del) str
+  item = takeWhile (/= del) noLeadingDelStr
+  restStr = dropWhile (/= del) noLeadingDelStr
