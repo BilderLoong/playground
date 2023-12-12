27265e7d5485ad3ca7114f9ddd1f9d2251bdd227
birudo <cokoryuu@gmail.com>
2023-12-12 13:42:23 +0800

    feat: ws message protocol

--- mpv_helper/packages/client/src/app/main.tsx
+++ mpv_helper/packages/client/src/app/main.tsx
@@ -1,15 +1,19 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { RefObject, useEffect, useRef } from "react";
+import { Command, keyMessage } from "server/protocols/ws";
 
 export const Main = (props: {}) => {
   const wsRef = useRef<WebSocket>();
   useEffect(() => {
     // TODO make the link dynamic.
-    const ws = new WebSocket("ws://localhost:8080");
+    const WS_ADDRESS = "ws://localhost:8080";
+
+    const ws = new WebSocket(WS_ADDRESS);
     wsRef.current = ws;
     const handleKeydown = (event: KeyboardEvent) => {
-      ws.send(event.key);
+      ws.send(keyMessageFactory(event.key));
     };
+
     document.addEventListener("keydown", handleKeydown);
 
     return () => {
@@ -20,3 +24,28 @@ export const Main = (props: {}) => {
 
   return <div>Click some key.</div>;
 };
+
+function keyMessageFactory(key: string) {
+  return JSON.stringify(
+    keyMessage.parse({
+      command: Command.key,
+      data: {
+        key,
+      },
+    }),
+  );
+}
+
+function keepConnect(
+  address: string,
+  ref: React.MutableRefObject<WebSocket | undefined>,
+): React.MutableRefObject<WebSocket | undefined> {
+  const ws = new WebSocket(address);
+  ref.current = ws;
+  // ws.onclose = function (ev) {
+  //   const { code } = ev;
+  //   console.log("WebSocket connection closed with: ", ev);
+  //   setTimeout(() => keepConnect(address, ref), 100);
+  // };
+  return ref;
+}
--- mpv_helper/packages/server/index.ts
+++ mpv_helper/packages/server/index.ts
@@ -1,7 +1,13 @@
 import Mpv from "mpv";
 import { WebSocketServer } from "ws";
-import { IncommingMessage, keyMessage, incommingMessage } from "./protocols/ws";
+import {
+  IncommingMessage,
+  keyMessage,
+  incommingMessage,
+  Command,
+} from "./protocols/ws";
 import { match, P } from "ts-pattern";
+import { createLanguageService } from "typescript";
 
 (async function () {
   const mpv = await Mpv({
@@ -14,11 +20,10 @@ import { match, P } from "ts-pattern";
     ],
   });
 
-  await mpv.command(
-    "loadfile",
-    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-    // "https://gotranscript.com/samples/captions-example.srt",
-  );
+  // await mpv.command(
+  //   "loadfile",
+  //   "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+  // );
 
   // await mpv.command(
   //   "sub-add",
@@ -32,7 +37,6 @@ import { match, P } from "ts-pattern";
     ws.on("error", console.error);
 
     ws.on("message", function message(data) {
-      console.log("received: %s", data);
       messageDispatcher(data.toString());
     });
 
@@ -41,14 +45,17 @@ import { match, P } from "ts-pattern";
 })();
 
 function messageDispatcher(wsMsg: string) {
-  const pareseRes = incommingMessage.safeParse(wsMsg);
-  if (!pareseRes.success) {
+  const pareseMsg = incommingMessage.safeParse(JSON.parse(wsMsg));
+  if (!pareseMsg.success) {
     return {
       message: `Unsupport message: ${wsMsg}`,
     };
   }
 
-  const { command } = pareseRes.data;
-  // WIP continue here
-  // match
+  match(pareseMsg.data).with(
+    { command: Command.key, data: { key: P._ } },
+    (key) => {
+      console.log({ key });
+    },
+  );
 }
