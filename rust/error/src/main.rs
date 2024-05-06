6d8e46ea580e09ffe3ecc10a6489485ab3106a5f
birudo <cokoryuu@gmail.com>
2024-05-06 14:16:38 +0800

    save

--- rust/error/src/main.rs
+++ rust/error/src/main.rs
@@ -11,4 +11,37 @@ fn read_username_from_file() -> Result<String, io::Error> {
 fn main() {
     let res = read_username_from_file().unwrap_or("123".to_string());
     print!("{}", res);
+    let x = 1;
+    ref_fn(&x);
+    normal_fn(x);
+}
+fn normal_fn(x: i32) {
+    println!("{}", x);
+}
+
+fn ref_fn_1(x: &i32) {
+    println!("{}", x);
+}
+
+fn ref_fn(&x: &i32) {
+    println!("{}", x);
+}
+
+#[derive(Debug)]
+struct Solution {}
+
+impl Solution {
+    pub fn next_greater_element(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        nums1
+            .iter()
+            .map(|x| {
+                *nums2
+                    .iter()
+                    .skip_while(|&y| y != x)
+                    .skip(1)
+                    .find(|&y| y > x)
+                    .unwrap_or(&-1)
+            })
+            .collect()
+    }
 }
