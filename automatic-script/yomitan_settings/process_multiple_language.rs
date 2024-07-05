6a611ccbb0ca290476b8e6317c4de8c993fc9850
birudo <cokoryuu@gmail.com>
2024-07-05 12:08:40 +0800

    save

--- automatic-script/yomitan_settings/process_multiple_language.rs
+++ automatic-script/yomitan_settings/process_multiple_language.rs
@@ -3,12 +3,28 @@
 //!
 //! ```cargo
 //! [dependencies]
-//! rand = "0.8.0"
+//! serde = { version = "1.0", features = ["derive"] }
+//! serde_json = "1.0"
 //! ```
 
-use rand::prelude::*;
+use serde::Deserialize;
+use serde_json::{Result, Value};
+use std::fs::read_to_string;
+use std::fs::File;
+use std::io::BufReader;
 
 fn main() {
     let x: u64 = random();
     println!("A random number: {}", x);
+}
+
+fn read_json_file(file_path: &str) -> Result {
+    // Open the file in read-only mode with buffer.
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    // Parse the JSON file into a serde_json::Value.
+    let json_value: Value = serde_json::from_reader(reader)?;
+
+    Ok(json_value)
 }
