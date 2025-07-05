3a319a1d5f8827311336f95efdd9ec0f85441e58
birudo <a514059172@gmail.com>
2025-07-05 10:56:38 +0800

    save

---
+++ tampermonkey_scripts/hide_translation_on_mainichi_nonbiri.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name          Remove Chinese translation and blur english tranlation on mainichi-nonbiri.com
+// @namespace     http://tampermonkey.net/
+// @version       2025-06-30
+// @description   try to take over the world!
+// @author        You
+// @match         https://mainichi-nonbiri.com/grammar/*
+// @icon          https://www.google.com/s2/favicons?sz=64&domain=mainichi-nonbiri.com
+// @grant         none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const style = document.createElement('style');
+    style.textContent = `
+        .gray {
+            filter: blur(5px); /* Initial blur */
+            transition: filter 0.3s ease-in-out; /* Smooth transition */
+        }
+
+        .gray {
+          display: none;
+        }
+
+        .gray:hover {
+            filter: blur(0px); /* Unblur on hover */
+        }
+    `;
+    document.head.appendChild(style);
+
+})();
