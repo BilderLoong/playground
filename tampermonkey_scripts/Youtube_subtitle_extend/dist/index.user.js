662979a4dc47bc5cf0213ca80ef9eb47414248b8
birudo <a514059172@gmail.com>
2025-09-12 21:55:27 +0800

    save

--- tampermonkey_scripts/Youtube_subtitle_extend/dist/index.user.js
+++ tampermonkey_scripts/Youtube_subtitle_extend/dist/index.user.js
@@ -1,7 +1,7 @@
-// ==UserScript=
+// ==UserScript==
 // @name         Language reactor subtitle extender
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.5
 // @description  So that Yomitan (or other popup dictionary) can pick up full sentence.
 // @author       Birudo
 // @match        *://www.youtube.com/watch*
