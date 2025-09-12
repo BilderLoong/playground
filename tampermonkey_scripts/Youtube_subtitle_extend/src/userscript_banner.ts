bfcd7e7e933d292d7c47cb5a0c6c9332c75f152b
birudo <a514059172@gmail.com>
2025-09-12 22:14:16 +0800

    save

--- tampermonkey_scripts/Youtube_subtitle_extend/src/userscript_banner.ts
+++ tampermonkey_scripts/Youtube_subtitle_extend/src/userscript_banner.ts
@@ -2,7 +2,8 @@ export const userScriptBanner = `
 // ==UserScript==
 // @name         Language reactor subtitle extender
 // @namespace    http://tampermonkey.net/
-// @version      1.6
+// @version      1.7
+// @license MIT
 // @description  So that Yomitan (or other popup dictionary) can pick up full sentence.
 // @author       Birudo
 // @match        *://www.youtube.com/watch*
