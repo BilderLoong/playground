73c870e9bb26350cf16d695f79f557f066295bea
birudo <a514059172@gmail.com>
2026-01-30 13:57:15 +0700

    save

--- tampermonkey_scripts/Remove Premium Ribbon on Windy.js
+++ tampermonkey_scripts/Remove Premium Ribbon on Windy.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @updateURL    https://raw.githubusercontent.com/BilderLoong/playground/refs/heads/master/tampermonkey_scripts/Remove%20Premium%20Ribbon%20on%20Windy.js
 // @downloadURL  https://raw.githubusercontent.com/BilderLoong/playground/refs/heads/master/tampermonkey_scripts/Remove%20Premium%20Ribbon%20on%20Windy.js
-// @version      1.1.0
+// @version      1.2.0
 // @description  Removes the "Get Premium" ribbon/element on Windy.com
 // @author       cokoryu@outlook.com
 // @match        https://www.windy.com/*
