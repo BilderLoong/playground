99dddd6fd2dd87f76574059b7d26aafc2d3deddb
birudo <a514059172@gmail.com>
2026-04-24 14:04:53 +0800

    save

---
+++ tampermonkey_scripts/Simpify_Xiaohongshu.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Simplify Xiaohongshu
+// @namespace    http://tampermonkey.net/
+// @updateURL    https://raw.githubusercontent.com/BilderLoong/playground/refs/heads/master/tampermonkey_scripts/Simpify_Xiaohongshu.js
+// @downloadURL  https://raw.githubusercontent.com/BilderLoong/playground/refs/heads/master/tampermonkey_scripts/Simpify_Xiaohongshu.js
+// @version      1.0.0
+// @description  Remove the feed page on Xiaohongshu.com;
+// @author       cokoryu@outlook.com
+// @match        https://www.xiaohongshu.com/explore/*
+// @grant        none
+// @license MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+     // Select the element containing the watermark using its CSS class
+    const style = document.createElement('style');
+    style.innerHTML = `
+    #exploreFeeds {
+       display: none !important;
+    }
+    `;
+    document.head.appendChild(style);
+})();
---
+++ tampermonkey_scripts/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+    printf("Found %s\n", "123");
+}
